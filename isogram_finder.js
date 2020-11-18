d0ec5bc1e87ae9d66dbe00311097621b86e70fc4
Neale Johnston <neale@team-johnston.com>
2020-11-18 20:59:09 +0000

    Tests Passing!

--- isogram_finder.js
+++ isogram_finder.js
@@ -1,9 +1,23 @@
 const IsogramFinder = function (word) {
+  this.word = word;
 
 }
 
 IsogramFinder.prototype.isIsogram = function () {
 
+  const wordArray = this.word.split('');
+  const wordArrayLowerCase = wordArray.map(letter => letter.toLowerCase());
+
+  function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+  }
+  const isogram = wordArrayLowerCase.filter(onlyUnique)
+
+  if (isogram.length === wordArrayLowerCase.length) {
+    return true
+  } else {
+    return false
+  }
 }
 
 module.exports = IsogramFinder;

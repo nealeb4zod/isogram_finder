8f975404c7c919df3873b2332bf18d25a4df0b52
Neale Johnston <neale@team-johnston.com>
2020-11-19 09:22:58 +0000

    Use set

--- isogram_finder.js
+++ isogram_finder.js
@@ -8,7 +8,7 @@ IsogramFinder.prototype.isIsogram = function () {
   const wordArray = this.word.toLowerCase().split('');
   const uniqueLettersList = [...new Set(wordArray)]
 
-  if (uniqueLettersList.length === wordArrayLowerCase.length) {
+  if (uniqueLettersList.length === wordArray.length) {
     return true
   } else {
     return false
